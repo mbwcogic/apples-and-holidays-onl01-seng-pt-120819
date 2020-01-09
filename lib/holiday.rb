500efb4536586ba88a7836be99466bae38840e39
Michael Waldo <mbwcogic@yahoo.com>
2020-01-09 11:30:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
 
    holiday_hash.collect do |season, holiday|
-    holiday.collect do |holiday, supplies|
+    holiday.collect do |holiday_name, supplies|
       
 end
 
